cda539cf4f97fb5a430477026d371a3842f8f16d
Zepp <zeppsoft@gmail.com>
2020-05-11 19:31:33 +0300

    Add project files.

---
+++ SUTODev2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SUTODev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SUTODev2!");
+        }
+    }
+}
