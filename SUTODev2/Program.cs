6f1d83ba63bfc75a48b4edee60dcef386b05898a
Viktor Shershnov <viktor.shershnov@quipu.de>
2020-10-27 16:44:09 +0200

    Collections Part 1.

--- SUTODev2/Program.cs
+++ SUTODev2/Program.cs
@@ -1,5 +1,8 @@
 ﻿using _1_Operators;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SUTODev2
 {
@@ -41,12 +44,48 @@ namespace SUTODev2
                 dis.Dispose();
             }*/
 
+            //Correct approach
+            /*
             using (var dis = new MyDisposable())
             {
                 dis.DoSomething();
                 //throw new Exception();
-            }
-            
+            }*/
+
+
+            #endregion
+
+            #region 5_InternalCollections
+
+            /*var l = new List<int>() { 4, 5, 6 };
+            var i = l as IEnumerable<int>;
+
+            var enumerator = i.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }*/
+
+            /*
+            //Comparing of two equal arrays by content
+            var arr = new int[]{1,2,9};
+            var arr2 = new int[] { 1, 2, 9 };
+
+            Console.WriteLine(arr.SequenceEqual(arr2));*/
+
+            //Can we resize the regular array?
+
+            var arr = new int[10];
+            arr[0] = 1;
+            arr[1] = 2;
+            Console.WriteLine(arr.Length);
+            Array.Resize(ref arr, 15);
+            Console.WriteLine(arr.Length);
+
+
+
+
 
             #endregion
 
